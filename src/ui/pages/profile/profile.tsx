733f64fd634d15e3276a770817f4bfaca5751a39
DimaMaslo88 <dmitrij-maslo@mail.ru>
2023-10-30 21:03:09 +0300

    "feat/user page "

--- src/ui/pages/profile/profile.tsx
+++ src/ui/pages/profile/profile.tsx
@@ -4,6 +4,8 @@ import {useSelector} from "react-redux";
 import {selectUserId, selectUserJob, selectUserName, selectUserPhotoSmall} from "bll/selectors/Selectors";
 import s from 'styles/profile.module.scss'
 import {Avatar} from "images/avatars/avatar";
+import {Settings} from "images/icons/settings";
+import {ToolTip} from "ui/components/universal/tooltip/ToolTip";
 
 
 export const Profile = () => {
@@ -20,22 +22,32 @@ export const Profile = () => {
     // },[])
     return (
         <div className={s.profileContainer}>
+
+            <div className={s.settings} >
+                <ToolTip text='Изменить настройки профиля'>
+            <Settings
+                width={50}
+                height={50}
+            />
+                </ToolTip>
+            </div>
+
             <h1> My profile</h1>
             <div className={s.avaBlock}>
-            <div className={s.avatar} >{photoLarge === null ?
-                <Avatar
-                    className={s.ava}
-                    width={150}
-                    height={150}
+                <div className={s.avatar}>{photoLarge === null ?
+                    <Avatar
+                        className={s.ava}
+                        width={150}
+                        height={150}
 
-                /> :
-                <div>
-                    photoLarge
+                    /> :
+                    <div>
+                        photoLarge
+                    </div>
+                }
+                    {!userJob ? "OpenToWork" : <h3>{userJob}</h3>}
                 </div>
-            }
-                {!userJob ? "OpenToWork" : <h3>{userJob}</h3> }
-            </div>
-            <h3> {userName}</h3>
+                <h3> {userName}</h3>
 
 
             </div>
