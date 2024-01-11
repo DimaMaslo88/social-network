c291008eb67ad2b0d5e64841c4fc608db5a2491b
DimaMaslo88 <dmitrij-maslo@mail.ru>
2024-01-11 21:34:57 +0300

    "feat/community page "

--- src/ui/pages/community/userPage/UserPage.tsx
+++ src/ui/pages/community/userPage/UserPage.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Avatar } from 'images/avatars/avatar';
+import style from 'styles/settings.module.scss';
+import s from 'styles/usersPage.module.scss'
 
-export const UserPage = () => {
-    return (
-        <div>
-
+type UserPageType = {
+  name: string;
+  followed: boolean;
+  status: null | string;
+  photo: null | string;
+};
+export const UserPage = ({ name, followed, status, photo }: UserPageType) => {
+  return (
+    <div>
+      <div>
+        {photo === null ? (
+          <Avatar className={style.ava} width={120} height={120} />
+        ) : (
+          <div>{photo}</div>
+        )}
+      </div>
+        <div className={s.textStyle}>
+      {name}
         </div>
-    );
+      {followed}
+    </div>
+  );
 };
 
